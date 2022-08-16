6dba3f0714d78594bed04922ba5cb7c828f1ab00
Ivan Kostadinov <ikostadinov@melontech.com>
2022-08-16 09:31:33 +0300

    MD-7 ADd Redux to Project * add Redux Store to project and export it * provide Redux Store in index.tsx * create Counter Component to test Redux Store

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store';
+import { CounterActions } from '../../store/counter';
 
 function Counter(): JSX.Element {
+  const [increaseValue, setIncreaseValue] = useState<number>(0);
+
+  // const counter = useSelector<RootState>((state: RootState) => state.counter.counter);
+  const counter = useSelector((state: RootState) => state.counter.counter);
+  // const showCounter = useSelector<RootState>((state: RootState) => state.counter.showCounter);
+  const showCounter = useSelector((state: RootState) => state.counter.showCounter);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const increaseValueHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setIncreaseValue(+event.target.value);
+  };
+
   return (
-    <div>Counter</div>
-  )
+    <React.Fragment>
+      {showCounter && <h2>Counter: {counter}</h2>}
+      <label htmlFor='value'>Choose Increase value:</label>
+      <input
+        type='number'
+        id='value'
+        value={increaseValue}
+        onChange={increaseValueHandler}
+        min='1'
+        max='10'
+        step='1'
+      />
+      <button onClick={() => dispatch(CounterActions.increment())}>+</button>
+      <button onClick={() => dispatch(CounterActions.decrement())}>-</button>
+      <button onClick={() => dispatch(CounterActions.increase(increaseValue))}>increase</button>
+      <button onClick={() => dispatch(CounterActions.toggleCounter())}>Toggle</button>
+    </React.Fragment>
+  );
 }
 
 export default Counter;
